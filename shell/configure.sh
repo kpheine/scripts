270e7b8c362e5450af82105746086c022dbadc81
Pietro DeLune <laccuvit@gmail.com>
2023-09-27 01:47:28 -0300

    pog

---
+++ shell/configure.sh
@@ -0,0 +1,3 @@
+
+sudo apt install $(cat ../utils/hackthebox_tools.list | tr "\n" " ") -y
+
---
+++ shell/terminal_config.sh
@@ -0,0 +1,2 @@
+# Custom terminal configuration (WIP)
+echo "PS1='\n-[\D{%Y-%m-%d} \t]-[\[\e[91m\]\u\[\e[0m\]@\[\e[92m\]\H\[\e[0m\]]\n-(\w) \$'" >> ~/.bashrc
